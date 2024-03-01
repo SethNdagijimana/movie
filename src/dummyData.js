99097ab1e8daba196ede50b3a3bd30bd60170cd4
Seth <seth@Seth.local>
2024-03-01 09:51:33 +0200

    movie stream

---
+++ src/dummyData.js
@@ -0,0 +1,189 @@
+export const homeData = [
+  {
+    id: 1,
+    name: "Moon Knight",
+    rating: 9.7,
+    time: "2hr : 22mins",
+    desc: "Steven Grant and mercenary Marc Spector investigate the mysteries of the Egyptian gods from inside the same body.",
+    starring: "Oscar Isaac, May Calamawy, Ethan Hawke",
+    genres: "Action,Sci-fi",
+    tags: "Action, Adventures, Horror",
+    cover: "../images/1.jpeg",
+    video: "https://www.youtube.com/watch?v=2GfBkC3qs78",
+    date: "20-Jan-1997"
+  },
+  {
+    id: 2,
+    name: "skull island",
+    rating: 8.6,
+    time: "2hr : 22mins",
+    desc: "A crew that reaches Skull Island to map it, is attacked by a humongous ape. The survivors then regroup to find out more about the ape, the island's natives and underground monsters.",
+    starring: "Oscar Isaac, May Calamawy, Ethan Hawke",
+    genres: "Adventures",
+    tags: "Adventures,Animation,Family",
+    cover: "../images/skull1.jpeg",
+    video: "https://www.youtube.com/watch?v=2GfBkC3qs78",
+    date: "10-JUL-2021"
+  },
+  {
+    id: 3,
+    name: "Black Hawk Down",
+    rating: 9.8,
+    time: "2hr : 22mins",
+    desc: "When an elite force of American soldiers is tasked with the mission to nab a Somalian warlord's top lieutenants, they find themselves ambushed by a large group of heavily-armed Somalis instead. ",
+    starring: "Josh Hartnett, Eric Bana, William Fichtner",
+    genres: "Action, War",
+    tags: "War,Action ",
+    cover: "../images/black.jpeg",
+    video: "https://www.youtube.com/watch?v=2GfBkC3qs78",
+    date: "20-FEB-2010"
+  },
+  {
+    id: 4,
+    name: "Sand Man",
+    rating: 6.9,
+    time: "2hr : 22mins",
+    desc: "A little girl with supernatural imagination powers brings to life a monster named Sandman, who protects her in times of distress.",
+    starring: "Tom Sturridge, Jenna Coleman, Boyd Holbrook",
+    genres: "Action",
+    tags: "Action, Adventures, Horror",
+    cover: "../images/sand1.jpg",
+    video: "https://www.youtube.com/watch?v=2GfBkC3qs78",
+    date: "12-Aug-2015"
+  }
+]
+export const upcome = [
+  {
+    id: 1,
+    cover: "../images/upcome/u1.jpg",
+    name: "My office Boss",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 2,
+    cover: "../images/upcome/u2.jpg",
+    name: "Shadowe",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 3,
+    cover: "../images/upcome/u3.jpg",
+    name: "Another Danger",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 4,
+    cover: "../images/upcome/u4.jpg",
+    name: "One Man Army",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 5,
+    cover: "../images/upcome/u5.jpg",
+    name: "Jumbo Queen",
+    time: "2hr : 38mins"
+  }
+]
+export const latest = [
+  {
+    id: 1,
+    cover: "../images/upcome/u6.png",
+    name: "King of Jungle",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 2,
+    cover: "../images/upcome/u7.jpg",
+    name: "The illusion",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 3,
+    cover: "../images/upcome/u1.jpg",
+    name: "Latest Movie",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 4,
+    cover: "../images/upcome/u5.jpg",
+    name: "Latest two",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 5,
+    cover: "../images/upcome/u4.jpg",
+    name: "Latest Three",
+    time: "2hr : 38mins"
+  }
+]
+
+export const trending = [
+  {
+    id: 1,
+    name: "The Dinosaur",
+    rating: 4.7,
+    time: "2hr : 22mins",
+    desc: "Sand and dust storms (SDS), also known as sirocco, haboob, yellow dust, white storms, and the harmattan, are a natural phenomenon linked with land and water management and climate change.",
+    starring: "Karen Gilchrist, James Earl Jones",
+    genres: "Action",
+    tags: "Action, Adventures, Horror",
+    cover: "../images/img/slide1.png",
+    date: "20-Jan-1997"
+  },
+  {
+    id: 2,
+    name: "The Great Emperor",
+    rating: 4.6,
+    time: "2hr : 22mins",
+    desc: "The island, like its inhabitants, is wholly fictional. Kong's island home in the Pacific makes its first appearance, along with Kong himself, in the 1933 film King Kong. ",
+    starring: "Brenda Chapman, Jeff Nathanson",
+    genres: "Adventures",
+    tags: "Adventures,Animation,Family",
+    cover: "../images/img/slide2.jpg",
+    date: "10-JUL-2021"
+  },
+  {
+    id: 3,
+    name: "Jumbo Queen",
+    rating: 4.8,
+    time: "2hr : 22mins",
+    desc: "Piracy is an act of robbery or criminal violence by ship or boat-borne attackers upon another ship or a coastal area, typically with the goal of stealing cargo and other valuable items or properties. ",
+    starring: "James Chinlund, Jeff Nathanson",
+    genres: "Adventures",
+    tags: "Adventures,Action ",
+    cover: "../images/img/slide3.jpg",
+    date: "20-FEB-2010"
+  }
+]
+export const recommended = [
+  {
+    id: 4,
+    cover: "../images/rec/rec1.jpg",
+    name: "One Man Army",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 5,
+    cover: "../images/rec/rec2.jpg",
+    name: "Jumbo Queen",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 1,
+    cover: "../images/rec/rec3.jpg",
+    name: "My office Boss",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 2,
+    cover: "../images/rec/rec4.jpg",
+    name: "Shadowe",
+    time: "2hr : 38mins"
+  },
+  {
+    id: 3,
+    cover: "../images/upcome/u3.jpg",
+    name: "Another Danger",
+    time: "2hr : 38mins"
+  }
+]
